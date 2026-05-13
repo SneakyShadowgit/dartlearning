015c62c288422a9a7fa7440b249e1a4a1a8f6a6b
hari <harikrishnaputhanpurayil@gmail.com>
2026-05-13 22:40:16 +0530

    fifth-commit

--- factorial.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  int n=5;
-  int fact=1;
-  while(n>0){
-    fact=fact*n;
-    n--;
-  }
-  print('fact : $fact');
-}
--- swap.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  int num1=20;
-  int num2=10;
-  num1 = num1+num2;
-  num2= num1-num2;
-  num1= num1-num2;
-  print( '$num1 and $num2' );
-}
